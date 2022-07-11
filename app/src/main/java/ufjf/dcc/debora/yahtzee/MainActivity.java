4f18fc384cdc57fa540b7de1b9d7207130855147
dduartevla <deboraduarte@ice.ufjf.br>
2022-07-10 21:01:20 -0300

    Criação do jogada_layout.xml.

--- app/src/main/java/ufjf/dcc/debora/yahtzee/MainActivity.java
+++ app/src/main/java/ufjf/dcc/debora/yahtzee/MainActivity.java
@@ -1,14 +1,26 @@
 package ufjf.dcc.debora.yahtzee;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerViewJogadas;
+    private RecyclerView recyclerViewDados;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerViewJogadas = findViewById(R.id.recyclerViewJogadas);
+        recyclerViewDados = findViewById(R.id.recyclerViewDados);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerViewDados.setLayoutManager(layoutManager);
+        recyclerViewJogadas.setLayoutManager(layoutManager);
     }
 }
