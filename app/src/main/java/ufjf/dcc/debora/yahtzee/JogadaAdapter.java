2e36ca273b584bd097dd921b112e67d5418a06f5
dduartevla <deboraduarte@ice.ufjf.br>
2022-07-15 13:03:10 -0300

    Criação da Classe JagadaAdapter (ainda não terminada), adição das imagens para os dados (ainda sem saber como vou fazer funcionar.

---
+++ app/src/main/java/ufjf/dcc/debora/yahtzee/JogadaAdapter.java
@@ -0,0 +1,68 @@
+package ufjf.dcc.debora.yahtzee;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class JogadaAdapter extends RecyclerView.Adapter<JogadaAdapter.JogadaViewHolder>{
+    private List<Jogada> jogadas;
+    private OnJogadaClickListener listener;
+
+    public JogadaAdapter(List<Jogada> avistamentos, OnJogadaClickListener listener) {
+        this.jogadas = avistamentos;
+        this.listener = listener;
+    }
+
+    @NonNull
+    @Override
+    public JogadaAdapter.JogadaViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context contexto = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(contexto);
+        View avistamentoView = inflater.inflate(R.layout.jogada_layout,parent,false);
+        JogadaViewHolder viewHolder = new JogadaViewHolder(avistamentoView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull JogadaAdapter.JogadaViewHolder holder, int position) {
+        Jogada jogada = jogadas.get(position);
+        holder.textViewJogadaNome.setText(jogada.getNome());
+        holder.textViewPontos.setText(jogada.getPontos());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return jogadas.size();
+    }
+
+    public class JogadaViewHolder extends RecyclerView.ViewHolder{
+
+        private TextView textViewJogadaNome;
+        private TextView textViewPontos;
+        private TextView textViewCalculaPontos;
+        private CheckBox checkBoxJogar;
+
+        public JogadaViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewJogadaNome = itemView.findViewById(R.id.textViewJogadaNome);
+            textViewPontos = itemView.findViewById(R.id.textViewPontos);
+            textViewCalculaPontos = itemView.findViewById(R.id.textViewCalculoPontos);
+            checkBoxJogar = itemView.findViewById(R.id.checkBoxJogar);
+        }
+    }
+
+    public interface OnJogadaClickListener{
+        void onJogadaClick(View view,int position);
+    }
+}
+
+
--- app/src/main/java/ufjf/dcc/debora/yahtzee/MainActivity.java
+++ app/src/main/java/ufjf/dcc/debora/yahtzee/MainActivity.java
@@ -4,18 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerViewJogadas;
     private RecyclerView recyclerViewDados;
+    private int[] dados;
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        dados = new int[5];
         recyclerViewJogadas = findViewById(R.id.recyclerViewJogadas);
         recyclerViewDados = findViewById(R.id.recyclerViewDados);
 
